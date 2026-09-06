945f55429225c0238ce13368cb3535ccf24ee518
kosif199022-jpg <kosif199022@gmail.com>
2026-09-06 11:53:42 +0300

    feat: wrap app with cloud persistence boundary

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import "./intelligence/pwa.js";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
+import { CloudPersistenceBoundary } from "./CloudPersistenceBoundary.jsx";
 import "./styles.css";
 import "./design-v66.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CloudPersistenceBoundary>
+      <App />
+    </CloudPersistenceBoundary>
   </React.StrictMode>,
 );
