5d924c6b6a2c60ad77ce3e25a3b71b4115756fce
kosif199022-jpg <kosif199022@gmail.com>
2026-09-10 13:24:59 +0300

    maintenance: wrap app with safe recovery boundary

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,18 @@ import "./intelligence/pwa.js";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.jsx";
 import { CloudPersistenceBoundary } from "./CloudPersistenceBoundary.jsx";
+import { AppErrorBoundary } from "./components/AppErrorBoundary.jsx";
 import "./styles.css";
 import "./design-v66.css";
 import "./space-cinematic.css";
+import "./app-recovery.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CloudPersistenceBoundary>
-      <App />
-    </CloudPersistenceBoundary>
+    <AppErrorBoundary>
+      <CloudPersistenceBoundary>
+        <App />
+      </CloudPersistenceBoundary>
+    </AppErrorBoundary>
   </React.StrictMode>,
 );
