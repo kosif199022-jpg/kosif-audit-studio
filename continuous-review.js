337afd76f69987d570858345f6457843904cfc9a
kosif199022-jpg <kosif199022@gmail.com>
2026-09-10 17:04:17 +0300

    feat: wire issue workspace into V5 bootstrap

--- continuous-review.js
+++ continuous-review.js
@@ -1,13 +1,14 @@
 import { store, derive } from './v5/continuous-store.js';
 import { initDocuments, renderDocuments, renderAnalysisMetrics, testGateway } from './v5/continuous-documents.js';
 import { initCouncil, renderCouncil, renderRequests } from './v5/continuous-council.js';
+import { initIssues, renderIssues } from './v5/continuous-issues.js';
 import { initReporting, renderNow, renderRecipe, renderCandidates, renderAdjustments, renderTrace, renderReport } from './v5/continuous-reporting.js';
 import { initCompletion, renderCompletion } from './v5/continuous-completion.js';
 import { initPersistence } from './v5/continuous-persistence.js';
 
 const $=s=>document.querySelector(s);let persistence=null;
-if(!document.querySelector('link[href="./freshness-specialists.css"]')){const link=document.createElement('link');link.rel='stylesheet';link.href='./freshness-specialists.css';document.head.append(link)}
-function renderAll(options={}){derive();renderNow();renderAnalysisMetrics();renderDocuments();renderRecipe();renderCouncil();renderRequests();renderCandidates();renderAdjustments();renderTrace();if(store.engagement.reports.length)renderReport();renderCompletion();if(options.persist!==false)persistence?.schedule()}
-initDocuments({renderAll});initCouncil({renderAll});initReporting({renderAll});initCompletion({renderAll});persistence=initPersistence({renderAll});
+for(const href of ['./freshness-specialists.css','./issue-workspace.css'])if(!document.querySelector(`link[href="${href}"]`)){const link=document.createElement('link');link.rel='stylesheet';link.href=href;document.head.append(link)}
+function renderAll(options={}){derive();renderNow();renderAnalysisMetrics();renderDocuments();renderRecipe();renderCouncil();renderRequests();renderIssues();renderCandidates();renderAdjustments();renderTrace();if(store.engagement.reports.length)renderReport();renderCompletion();if(options.persist!==false)persistence?.schedule()}
+initDocuments({renderAll});initCouncil({renderAll});initIssues({renderAll});initReporting({renderAll});initCompletion({renderAll});persistence=initPersistence({renderAll});
 $('#reviewDocumentCard').onclick=()=>$('#documentInput').click();
 await persistence.hydrate();renderAll({persist:false});testGateway();
