2f1cd8e843bd9386f135a025a9c4994d216666ce
kosif199022-jpg <kosif199022@gmail.com>
2026-09-13 01:48:55 +0300

    feat: surface review command center in app shell

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import "./intelligence/pwa.js";
 import { createRoot } from "react-dom/client";
 import { AppErrorBoundary } from "./components/AppErrorBoundary.jsx";
 import { OperatingModelGuide } from "./components/OperatingModelGuide.jsx";
+import { ReviewCommandCenter } from "./components/ReviewCommandCenter.jsx";
 import "./app-recovery.css";
 import "./operating-model.css";
+import "./review-command-center.css";
 
 const App = lazy(() => import("./ResetApp.jsx").then((module) => ({ default: module.ResetApp })));
 const ResetVoiceLauncher = lazy(() => import("./components/ResetVoiceLauncher.jsx").then((module) => ({ default: module.ResetVoiceLauncher })));
@@ -34,6 +36,7 @@ createRoot(document.getElementById("root")).render(
         <ResetVoiceLauncher />
       </Suspense>
       <OperatingModelGuide />
+      <ReviewCommandCenter />
     </AppErrorBoundary>
   </React.StrictMode>,
 );
