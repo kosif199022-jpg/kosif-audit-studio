586b150f622aec6f0de8019d99597410fa6c1664
kosif199022-jpg <kosif199022@gmail.com>
2026-09-12 22:50:32 +0300

    feat: expose unified KOSIF operating flow in app

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,9 @@ import React, { Suspense, lazy } from "react";
 import "./intelligence/pwa.js";
 import { createRoot } from "react-dom/client";
 import { AppErrorBoundary } from "./components/AppErrorBoundary.jsx";
+import { OperatingModelGuide } from "./components/OperatingModelGuide.jsx";
 import "./app-recovery.css";
+import "../operating-model.css";
 
 const App = lazy(() => import("./ResetApp.jsx").then((module) => ({ default: module.ResetApp })));
 const ResetVoiceLauncher = lazy(() => import("./components/ResetVoiceLauncher.jsx").then((module) => ({ default: module.ResetVoiceLauncher })));
@@ -31,6 +33,7 @@ createRoot(document.getElementById("root")).render(
       <Suspense fallback={null}>
         <ResetVoiceLauncher />
       </Suspense>
+      <OperatingModelGuide />
     </AppErrorBoundary>
   </React.StrictMode>,
 );
